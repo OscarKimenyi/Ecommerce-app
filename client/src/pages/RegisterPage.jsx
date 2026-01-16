580231f1fcff6dc199789552b9965c27f9da3cbd
oscar <oscarollie366@gmail.com>
2026-01-16 15:52:15 +0300

    Create Register page component

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+
+const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Register submitted");
+  };
+
+  return (
+    <Row className="justify-content-md-center">
+      <Col xs={12} md={6}>
+        <h1>Sign Up</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name" className="my-3">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group controlId="email" className="my-3">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group controlId="password" className="my-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group controlId="confirmPassword" className="my-3">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </Form.Group>
+
+          <Button type="submit" variant="primary">
+            Register
+          </Button>
+        </Form>
+
+        <Row className="py-3">
+          <Col>
+            Have an Account? <Link to="/login">Login</Link>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+};
+
+export default RegisterPage;
