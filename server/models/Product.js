1ea58430f228363e2b987e84b143d344542097de
oscar <oscarollie366@gmail.com>
2026-01-17 09:23:24 +0300

    Update Product.js file

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,25 +1,14 @@
 import mongoose from "mongoose";
 
-const reviewSchema = new mongoose.Schema(
+const reviewSchema = mongoose.Schema(
   {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
     user: {
       type: mongoose.Schema.Types.ObjectId,
+      required: true,
       ref: "User",
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    rating: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 5,
-    },
-    comment: {
-      type: String,
-      required: true,
     },
   },
   {
@@ -27,9 +16,26 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
-const productSchema = new mongoose.Schema(
+const productSchema = mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
       type: String,
       required: true,
     },
@@ -37,34 +43,26 @@ const productSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
     price: {
       type: Number,
       required: true,
-      min: 0,
-    },
-    category: {
-      type: String,
-      required: true,
-    },
-    images: [
-      {
-        type: String,
-      },
-    ],
-    stock: {
-      type: Number,
-      required: true,
-      min: 0,
       default: 0,
     },
-    rating: {
+    countInStock: {
       type: Number,
+      required: true,
       default: 0,
-    },
-    reviews: [reviewSchema],
-    featured: {
-      type: Boolean,
-      default: false,
     },
   },
   {
@@ -72,4 +70,6 @@ const productSchema = new mongoose.Schema(
   }
 );
 
-export default mongoose.model("Product", productSchema);
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
