06ec463b921fb654800423dd470eb6733e510a87
oscar <oscarollie366@gmail.com>
2026-01-17 10:12:25 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { Container } from "react-bootstrap";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import HomePage from "./pages/HomePage";
-// import ProductPage from "./pages/ProductPage";
-// import CartPage from "./pages/CartPage";
-// import LoginPage from "./pages/LoginPage";
-// import RegisterPage from "./pages/RegisterPage";
-// import ProfilePage from "./pages/ProfilePage";
-// import ShippingPage from "./pages/ShippingPage";
-// import PaymentPage from "./pages/PaymentPage";
-// import PlaceOrderPage from "./pages/PlaceOrderPage";
-// import OrderPage from "./pages/OrderPage";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Header />
-//       <main className="py-3">
-//         <Container>
-//           <Routes>
-//             <Route path="/" element={<HomePage />} />
-//             <Route path="/product/:id" element={<ProductPage />} />
-//             <Route path="/cart" element={<CartPage />} />
-//             <Route path="/login" element={<LoginPage />} />
-//             <Route path="/register" element={<RegisterPage />} />
-//             <Route path="/profile" element={<ProfilePage />} />
-//             <Route path="/shipping" element={<ShippingPage />} />
-//             <Route path="/payment" element={<PaymentPage />} />
-//             <Route path="/placeorder" element={<PlaceOrderPage />} />
-//             <Route path="/order/:id" element={<OrderPage />} />
-//           </Routes>
-//         </Container>
-//       </main>
-//       <Footer />
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
@@ -59,7 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/product/:id" element={<ProductPage />} />
-            <Route path="/cart/:id?" element={<CartPage />} />
+            <Route path="/cart" element={<CartPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
           </Routes>
