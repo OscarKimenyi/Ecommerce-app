e60e0623b4b13a9534dc11a3b35c3d62d3f5b71c
oscar <oscarollie366@gmail.com>
2026-01-17 09:10:08 +0300

    update the main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,11 @@
-import { StrictMode } from "react";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
