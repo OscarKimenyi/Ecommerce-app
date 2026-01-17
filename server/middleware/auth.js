f5a80b616c937ab7d9cf7221eb1ed62896f225c0
oscar <oscarollie366@gmail.com>
2026-01-17 09:25:23 +0300

    Update auth.js file

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -10,10 +10,14 @@ export const protect = async (req, res, next) => {
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
+
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
       req.user = await User.findById(decoded.id).select("-password");
+
       next();
     } catch (error) {
+      console.error(error);
       res.status(401).json({ message: "Not authorized, token failed" });
     }
   }
@@ -27,6 +31,6 @@ export const admin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
   } else {
-    res.status(401).json({ message: "Not authorized as admin" });
+    res.status(401).json({ message: "Not authorized as an admin" });
   }
 };
